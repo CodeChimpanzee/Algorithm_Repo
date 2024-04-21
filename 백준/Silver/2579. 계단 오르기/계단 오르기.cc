8101527f75d99be826b5c23a2bc1f0a30cd506f6
CodeChimpanzee <77719074+CodeChimpanzee@users.noreply.github.com>
2024-04-22 00:09:57 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int dp[301][3];
+int dp[301];
 int stairs[301];
 int n;
 
@@ -11,20 +11,21 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int sum = 0;
     cin >> n;
     for(int i = 1; i <= n; i++)
-        cin >> stairs[i];
-
-    dp[1][1] = stairs[1];
-    dp[1][2] = -1;
-    dp[2][1] = stairs[2];
-    dp[2][2] = stairs[1] + stairs[2];
-
-    for(int i = 3; i <= n; i++)
     {
-        dp[i][1] = max(dp[i - 2][1], dp[i - 2][2]) + stairs[i];
-        dp[i][2] = dp[i - 1][1] + stairs[i];
+        cin >> stairs[i];
+        sum += stairs[i];
     }
 
-    cout << max(dp[n][1], dp[n][2]) << "\n";
+    dp[0] = 0;
+    dp[1] = stairs[1];
+    dp[2] = stairs[2];
+    for(int i = 3; i < n; i++)
+    {
+        dp[i] = min(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i]);
+    }
+
+    cout << sum - min(dp[n - 1], dp[n - 2]) << "\n";
 }
