038a17081a75aa1679b39ae1121cb8c7ae933a76
CodeChimpanzee <77719074+CodeChimpanzee@users.noreply.github.com>
2024-04-29 17:29:27 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 12 ms, Memory: 9852 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+int dp[1001][1001];
+int dat[1001][1001];
+
+int n, m;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++)
+    {
+        string t; cin >> t;
+        for(int j = 1; j <= m; j++)
+            dat[i][j] = t[j - 1] - '0';
+    }
+
+    int ans = 0;
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= m; j++)
+            if(dat[i][j] == 1)
+            {
+                dp[i][j] = min({dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]}) + 1;
+                ans = max(ans, dp[i][j]);
+            }
+
+     cout << ans * ans << "\n";
+}
