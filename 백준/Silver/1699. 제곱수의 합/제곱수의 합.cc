2c5e8a812440cdca207177e989071423d95d19bd
CodeChimpanzee <77719074+CodeChimpanzee@users.noreply.github.com>
2024-04-29 15:47:42 +0900

    [Silver II] Title: 제곱수의 합, Time: 28 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -11,8 +11,8 @@ int main()
     fill(dp, dp + n + 1, 1000);
     
     dp[0] = 0;
-    for(int i = 1; i <= n; i++)
-        for(int j = 1; j <= 316; j++)
+    for(int j = 1; j <= 316; j++)
+        for(int i = 1; i <= n; i++)
             if(i >= j * j)
                 dp[i] = min(dp[i], dp[i - j * j] + 1);
 
