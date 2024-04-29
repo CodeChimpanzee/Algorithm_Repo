ab48895a69c4462f01f6b5904c5702ee2185a64c
CodeChimpanzee <77719074+CodeChimpanzee@users.noreply.github.com>
2024-04-29 15:42:27 +0900

    [Silver II] Title: 제곱수의 합, Time: 64 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int dp[100001];
+int n;
+
+int main()
+{
+    cin >> n;
+    fill(dp, dp + 100001, 1000);
+    
+    dp[0] = 0;
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= 316; j++)
+            if(i >= j * j)
+                dp[i] = min(dp[i], dp[i - j * j] + 1);
+
+    cout << dp[n] << "\n"; 
+}
