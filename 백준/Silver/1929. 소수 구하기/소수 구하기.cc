c636da23d170124227756cea8713002197519852
CodeChimpanzee <77719074+CodeChimpanzee@users.noreply.github.com>
2024-05-02 16:55:49 +0900

    [Silver III] Title: 소수 구하기, Time: 384 ms, Memory: 2928 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+
+using namespace std;
+
+vector<int> prime;
+int m, n;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> m >> n;
+
+    prime.push_back(2);
+    for(int i = 3; i <= n; i++)
+    {
+        bool is_p = true;
+        int lim = (int)sqrt(i);
+        for(int p : prime)
+        {
+            if(i % p == 0)
+                is_p = false;
+            if(p > lim)
+                break;
+        }
+        if(is_p)
+            prime.push_back(i);
+    }
+
+    for(int p : prime)
+    {
+        if(p < m) continue;
+        cout << p << "\n";
+    }
+}
