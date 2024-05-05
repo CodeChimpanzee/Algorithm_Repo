b0215a2030f5ac7fe17a8b3279d6c436290782e8
CodeChimpanzee <77719074+CodeChimpanzee@users.noreply.github.com>
2024-05-06 00:12:30 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -10,18 +10,18 @@ ll parametric_search(ll st, ll ed, ll tgt)
 {
     ll mid, mn;
 
-    while(st < ed)
+    while(st + 1 < ed)
     {
-        mid = (st + ed + 1) / 2;
+        mid = (st + ed) / 2;
 
         mn = 0;
         for(int i = 0; i < k; i++)
             mn += cable[i] / mid;
 
-        if(mn >= tgt)
+        if(mn > tgt)
             st = mid;
         else
-            ed = mid - 1;
+            ed = mid;
     }
 
     return st;
@@ -32,10 +32,10 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    ll imax = (1 << 31) - 1;
+    ll imax = (1LL << 31);
     cin >> k >> n;
     for(int i = 0; i < k; i++)
         cin >> cable[i];
     
-    cout << parametric_search(1, imax, n);
+    cout << parametric_search(1, imax, n - 1);
 }
