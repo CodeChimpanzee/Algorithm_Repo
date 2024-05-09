125e058cf72a362146f314171f16cf4f9bf254e6
CodeChimpanzee <77719074+CodeChimpanzee@users.noreply.github.com>
2024-05-09 18:47:07 +0900

    [Gold IV] Title: 알 수 없는 번호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1338. 알 수 없는 번호/알 수 없는 번호.cc
+++ 백준/Gold/1338. 알 수 없는 번호/알 수 없는 번호.cc
@@ -8,18 +8,14 @@ ll l, r, x, y;
 int main()
 {
     cin >> l >> r >> x >> y;
-    if(l > r) swap(l, r);
-    x = abs(x);
-    if(!(0 <= y && y < x)) cout << "Unknwon Number\n";
-    else
-    {
-        ll lq = l / x; // l = lq * x + lmod
-        if(l < 0) lq--;
+    if(l > r) swap(l, r); x = abs(x);
+    
+    ll lq = l / x;
+    if(l < 0) lq--;
 
-        ll nt = lq * x + y;
-        if(nt < l) nt += x;
+    ll nt = lq * x + y;
+    if(nt < l) nt += x;
 
-        if(nt > r || nt + x <= r) cout << "Unknwon Number\n";
-        else cout << nt << "\n";
-    }
+    if(nt > r || nt + x <= r || 0 > y || y >= x) cout << "Unknwon Number\n";
+    else cout << nt << "\n";
 }
