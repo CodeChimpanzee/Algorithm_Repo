56b17fd1cba0b1c4fdc8193cbb9be942ac3d15aa
CodeChimpanzee <77719074+CodeChimpanzee@users.noreply.github.com>
2024-05-28 00:50:35 +0900

    [Bronze II] Title: 분산처리, Time: 80 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.cc
+++ 백준/Bronze/1009. 분산처리/분산처리.cc
@@ -2,15 +2,20 @@
 
 using namespace std;
 
+int pow_seg(int a, int b)
+{
+    if(b == 1) return a % 10;
+    int mp = (b & 1 ? a : 1);
+    return (pow_seg(a, b/2) * pow_seg(a, b/2) * mp) % 10; 
+}
+
 int main()
 {
     int t; cin >> t;
     while(t--)
     {
         int a, b; cin >> a >> b;
-        int ans = 1;
-        for(int i = 1; i <= b; i++)
-            ans = (ans * a) % 10;
+        int ans = pow_seg(a, b);
         cout << (ans == 0 ? 10 : ans) << "\n";
     }
 }
