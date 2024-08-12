3cee54e314359743ee13f35768b3e56dccf2e3ef
CodeChimpanzee <77719074+CodeChimpanzee@users.noreply.github.com>
2024-08-12 22:04:00 +0900

    [D4] Title: 정사각형 방, Time: 184 ms, Memory: 33,400 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -1,11 +1,8 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Solution {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		
@@ -22,8 +19,8 @@ public class Solution {
 			}
 			
 			int max_num = 1;
-			int max_len = 1;
-			L : for(int i = 1; i < N * N; i++) {
+			int max_len = 0;
+			for(int i = 1; i < N * N; i++) {
 				int cur = i;
 				int cur_len = 1;
 				while(prox(coord[i], coord[i + 1])) {
